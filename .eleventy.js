2c1b30fcabb4aa4d3a06ced16d98d8ea98a9a569
MWD-Art <marc@mwd-art.de>
2024-06-27 15:55:58 +0200

    added template

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
-
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const esbuild = require('esbuild');
 module.exports = function (eleventyConfig) {
+  eleventyConfig.on('eleventy.before', async () => {
+    await esbuild.build({
+      entryPoints: ['src/js/index.js'],
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      outfile: 'public/js/index.js',
+    });
+  });
+
+  eleventyConfig.addNunjucksFilter('limit', (arr, limit) =>
+    arr.slice(0, limit)
+  );
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(eleventySass);
+
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addWatchTarget('./src/assets');
+  eleventyConfig.addWatchTarget('./src/js');
+
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => { });
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
